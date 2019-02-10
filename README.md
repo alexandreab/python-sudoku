143e7385b30272a55d2389098e080f3ce3309a5b
Alexandre Barbosa <alexandreab@live.com>
2019-02-10 17:13:56 -0200

    Add initial tests and CI settings
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-sudoku
+
+[![Build Status](https://travis-ci.com/alexandreab/python-sudoku.svg?branch=master)](https://travis-ci.com/alexandreab/python-sudoku)
+
+[![codecov](https://codecov.io/gh/alexandreab/python-sudoku/branch/master/graph/badge.svg)](https://codecov.io/gh/alexandreab/python-sudoku)
---
+++ tests.py
@@ -0,0 +1,43 @@
+import unittest
+from queue import LifoQueue
+
+from sudoku import Grid
+from search_tree import Searcher
+
+class TestSearchThree(unittest.TestCase):
+
+    def test_searcher(self):
+        data = [
+            [5, 3, 0,    0, 7, 0,    0, 0, 0],
+            [6, 0, 0,    1, 9, 5,    0, 0, 0],
+            [0, 9, 8,    0, 0, 0,    0, 6, 0],
+
+            [8, 0, 0,    0, 6, 0,    0, 0, 3],
+            [4, 0, 0,    8, 0, 3,    0, 0, 1],
+            [7, 0, 0,    0, 2, 0,    0, 0, 6],
+
+            [0, 6, 0,    0, 0, 0,    2, 8, 0],
+            [0, 0, 0,    4, 1, 9,    0, 0, 5],
+            [0, 0, 0,    0, 8, 0,    0, 7, 9],
+        ]
+        solution_data = [
+            [5, 3, 4,    6, 7, 8,    9, 1, 2],
+            [6, 7, 2,    1, 9, 5,    3, 4, 8],
+            [1, 9, 8,    3, 4, 2,    5, 6, 7],
+
+            [8, 5, 9,    7, 6, 1,    4, 2, 3],
+            [4, 2, 6,    8, 5, 3,    7, 9, 1],
+            [7, 1, 3,    9, 2, 4,    8, 5, 6],
+
+            [9, 6, 1,    5, 3, 7,    2, 8, 4],
+            [2, 8, 7,    4, 1, 9,    6, 3, 5],
+            [3, 4, 5,    2, 8, 6,    1, 7, 9],
+        ]
+        grid = Grid(data)
+        p = Searcher(grid, LifoQueue)
+        p.play(max_iter=200)
+        solution = p.solved.get()
+        self.assertEqual(solution.data, solution_data)
+
+if __name__ == '__main__':
+    unittest.main()
